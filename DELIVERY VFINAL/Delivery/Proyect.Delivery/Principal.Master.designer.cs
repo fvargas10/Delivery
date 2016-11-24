5bdf4c1f2f3682884d0a4d80bcef6e2fe0cfab3d
Francisco <panxofernando@gmail.com>
2016-11-24 17:14:24 -0300

    ponte vio

--- DELIVERY VFINAL/Delivery/Proyect.Delivery/Principal.Master.designer.cs
+++ DELIVERY VFINAL/Delivery/Proyect.Delivery/Principal.Master.designer.cs
@@ -31,6 +31,15 @@ namespace Proyect.Delivery {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Control Body.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Body;
+        
+        /// <summary>
         /// Control ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
--- DELIVERY VFINAL/Delivery/Proyect.Delivery/WebForm1.aspx
+++ DELIVERY VFINAL/Delivery/Proyect.Delivery/WebForm1.aspx
@@ -1,5 +1,9 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Proyect.Delivery.WebForm1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    gfdf
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 </asp:Content>
+
+
